045ee77a1ae43d705bcbc3b79eb2d5d49305b083
Miroslaw Staron <miroslaw.staron@ituniv.se>
2023-06-08 08:23:49 +0200

    Update start.py
    /copilot gentest

--- tests/start.py
+++ tests/start.py
@@ -7,3 +7,19 @@ def r12(a):
 
 def rev_str(strA):
   return strA [::-1]
+
+
+# new function to test if a year is a leap year
+def rev_str(strA):
+  return strA [::-1]
+
+
+def check_leap_year(intYear):
+  if (year % 400 == 0) and (year % 100 == 0):
+      return True
+
+  elif (year % 4 ==0) and (year % 100 != 0):
+      return False
+
+  else:
+      return True
